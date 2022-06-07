47175252eab06a76076f5d5ff467eb85e8a0066f
Pradhan <asitkumar.pradhan@thomsonreuters.com>
2022-06-07 16:09:04 +0530

    Added edit medal screen

--- src/medal_app/medal_edit/views.cljs
+++ src/medal_app/medal_edit/views.cljs
@@ -8,20 +8,24 @@
 (defn medal-edit []
   (let [route-params @(re-frame/subscribe [::root-subs/route-params])
         medal @(re-frame/subscribe [::subs/medal (:id route-params)])]
-    [:div
-     [:h1 (str "Edit medals" )]
-     [:form
-      [:lable (str "Country: " (:code medal))]
-      [:br]
-      [:lable "Gold "]
-      [:input {:type "number" :value (:gold medal)}]
-      [:br]
-      [:lable "Silver "]
-      [:input {:type "number" :value (:silver medal)}]
-      [:br]
-      [:lable "Bronze "]
-      [:input {:type "number" :value (:bronze medal)}]
-      [:br]
-      [:input {:type "submit" :value "Submit"} ]]]))
+    [:div.container
+     [:div.form-container
+      [:h1 (str "Edit medals")]
+      [:form {:method "POST"}
+       [:lable (str "Country: ")]
+       [:span
+        [:span {:class (str "flag-" (:code medal))}]]
+       [:span {:class "country"} (:code medal)]
+       [:br]
+       [:lable "Gold "]
+       [:input {:type "number" :value (:gold medal) :id "gold" :name "gold"}]
+       [:br]
+       [:lable "Silver "]
+       [:input {:type "number" :value (:silver medal) :id "silver" :name "silver"}]
+       [:br]
+       [:lable "Bronze "]
+       [:input {:type "number" :value (:bronze medal) :id "bronze" :name "bronze"}]
+       [:br]
+       [:input {:type "submit" :value "Submit"}]]]]))
 
 (defmethod routes/panels :medal-edit-panel [] [medal-edit])
